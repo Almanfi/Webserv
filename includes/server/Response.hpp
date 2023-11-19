e92a223e6936145baf920b2527bb930cce23dc7d
Almanfi <nassimed.dev@gmail.com>
2023-11-19 03:41:14 +0100

    backup

--- includes/server/Response.hpp
+++ includes/server/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: elasce <elasce@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 17:50:59 by maboulkh          #+#    #+#             */
-/*   Updated: 2023/11/18 21:46:19 by elasce           ###   ########.fr       */
+/*   Updated: 2023/11/19 03:29:00 by elasce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,63 +20,111 @@ class Response
 {
 private:
     std::map<std::string, std::string> ResponseHeader;
+    bool        isFile;
     std::string header;
-    std::string body;
-    std::string fileName;
+    // std::string body;
+    // std::string fileName;
     std::string code;
+    std::string data;
 
     std::string findType(std::string& fileName);
     std::string date();
     std::string contentLength();
 public:
-    Response(std::string& body);
+    // Response(std::string& body);
+    Response(std::string& code, std::stiring& data, bool isFile);
     ~Response();
 
 };
 
-Response::Response(std::string& body)
-{
-    StatusCode a;
+	// code[100] = "Continue";
+	// code[200] = "OK";
+	// code[201] = "Created";
+	// code[204] = "No Content";
+	// code[400] = "Bad Request";
+	// code[403] = "Forbidden";
+	// code[404] = "Not Found";
+	// code[405] = "Method Not Allowed";
+	// code[413] = "Payload Too Large";
+	// code[500] = "Internal Server Error";
 
-    fileName = "";
-    code = "200";
-    std::string StatusLine = a.statusLine(code);
-    // "HTTP-Version Status-Code Reason-Phrase\r\n"; //server.httpVer() + " " + status + phrase(status) + "\r\n";
+Response::Response(std::string& code, std::string& data, bool isFile) : code(code), data(data), isFile(isFile) {
     ResponseHeader["Server"] = "webserv"; // sever.name();
     ResponseHeader["date"] = date(); // date();
-    ResponseHeader["content-type"] = findType(fileName); // content.findType();
-    ResponseHeader["content-length"] = contentLength(); // content.lenth();
+    if (isFile) {
+        ResponseHeader["content-type"] = findType(data); // content.findType();
+        ResponseHeader["content-length"] = contentLength(); // content.lenth();
+    }
+    else {
+        ResponseHeader["content-type"] = findType(".txt"); // content.findType();
+        ResponseHeader["content-length"] = data.length(); // content.lenth();
+    }
+}
 
-/*
-HTTP/1.1 200 OK
-date: Fri, 17 Nov 2023 16:48:57 GMT
-content-type: text/html; charset=utf-8
-content-length: 1017
-x-request-id: a1bce949-f6af-4226-92a1-d746484b8a79
-cache-control: no-store, max-age=0
-accept-ch: sec-ch-prefers-color-scheme
-critical-ch: sec-ch-prefers-color-scheme
-vary: sec-ch-prefers-color-scheme
-x-adblock-key: MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANDrp2lz7AOmADaN8tA50LsWcjLFyQFcb/P2Txc58oYOeILb3vBw7J6f4pamkAQVSQuqYsKx3YzdUHCvbVZvFUsCAwEAAQ==_HDSApIK32dE8tmarsVRiNoDCy6ChdPP5Uy8Jjf116Tqg7be9w+AIiANsZh2PU+muk1bDI2ehY575ZFEH0KlcHA==
-set-cookie: parking_session=a1bce949-f6af-4226-92a1-d746484b8a79; expires=Fri, 17 Nov 2023 17:03:57 GMT; path=/
-*/
 
+// Response::Response(std::string& body)
+// {
+//     StatusCode a;
+
+//     fileName = "";
+//     code = "200";
+//     std::string StatusLine = a.statusLine(code);
+//     // "HTTP-Version Status-Code Reason-Phrase\r\n"; //server.httpVer() + " " + status + phrase(status) + "\r\n";
+//     ResponseHeader["Server"] = "webserv"; // sever.name();
+//     ResponseHeader["date"] = date(); // date();
+//     ResponseHeader["content-type"] = findType(fileName); // content.findType();
+//     ResponseHeader["content-length"] = contentLength(); // content.lenth();
+
+// /*
+// HTTP/1.1 200 OK
+// date: Fri, 17 Nov 2023 16:48:57 GMT
+// content-type: text/html; charset=utf-8
+// content-length: 1017
+// x-request-id: a1bce949-f6af-4226-92a1-d746484b8a79
+// cache-control: no-store, max-age=0
+// accept-ch: sec-ch-prefers-color-scheme
+// critical-ch: sec-ch-prefers-color-scheme
+// vary: sec-ch-prefers-color-scheme
+// x-adblock-key: MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANDrp2lz7AOmADaN8tA50LsWcjLFyQFcb/P2Txc58oYOeILb3vBw7J6f4pamkAQVSQuqYsKx3YzdUHCvbVZvFUsCAwEAAQ==_HDSApIK32dE8tmarsVRiNoDCy6ChdPP5Uy8Jjf116Tqg7be9w+AIiANsZh2PU+muk1bDI2ehY575ZFEH0KlcHA==
+// set-cookie: parking_session=a1bce949-f6af-4226-92a1-d746484b8a79; expires=Fri, 17 Nov 2023 17:03:57 GMT; path=/
+// */
+
+// }
+
+std::string Response::header() {
+    std:: string header;
+    char *SP = "\n";
+    StatusCode a;
+
+    header = a.statusLine(code);
+    header += SP;
+    for (std::map<std::string, std::string>::iterator it = ResponseHeader.begin(); *it != ResponseHeader.end(), i++) {
+        header += it->first + ": " + it->second + SP;
+    }
+    header += SP;
+    return (header);
 }
 
 std::string Response::contentLength() {
     size_t size;
     std::stringstream ss;
-    std::ifstream file(fileName.c_str());
-    if (!file.is_open())
-    {
-        // throw
-        return "";
+    if (isFile) {
+        std::ifstream file(data.c_str());
+        if (!file.is_open())
+        {
+            // throw
+            code = "500";
+            return "";
+        }
+        std::streampos start = file.tellg();
+        file.seekg(0, std::ios::end);
+        std::streampos end = file.tellg(); 
+        size = end - start
+        file.close();
     }
-    
-    std::streampos start = file.tellg();
-    file.seekg(0, std::ios::end);
-    std::streampos end = file.tellg(); 
-    ss << (end - start);
+    else 
+        size = data.length();
+    ss << size;
     return ((std::string) ss.str());
 }
 
